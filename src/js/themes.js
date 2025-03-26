0893529303c821cc9eace85d57c44fd801323d31
Larissasthefany <larigithub@gmail.com>
2025-03-26 19:42:52 -0300

    Adicionando temas

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,14 +1,38 @@
 const savedMode = localStorage.getItem("mode");
-if (savedMode) {
-  document.documentElement.dataset.mode = savedMode;
-  // Atualizar o estado do checkbox conforme o modo salvo
-  document.querySelector(".toggle input").checked = savedMode === "dark";
+
+const themes = {
+  dark: {
+    backgroundColor: "#333",
+    color: "#d3d3d3",
+  },
+
+  light: {
+    backgroundColor: "#fff",
+    color: "#a2a2a2",
+  },
+};
+
+function updateTheme(mode) {
+  const theme = themes[mode];
+
+  if (theme) {
+    Object.keys(theme).forEach((key) => {
+      document.body.style[key] = theme[key]; 
+      document.querySelector("main").style[key] = theme[key]; 
+    });
+  }
 }
 
-// Ouvir as mudanças no toggle e salvar o modo no localStorage
+// Aplica o tema salvo quando a página for carregada
+if (savedMode) {
+  // Atualiza o estado do checkbox conforme o modo salvo
+  document.querySelector(".toggle input").checked = savedMode === "dark";
+  updateTheme(savedMode);
+}
+
+// Ouve as mudanças no toggle e salva o modo no localStorage
 document.querySelector(".toggle input").addEventListener("change", (event) => {
   const mode = event.currentTarget.checked ? "dark" : "light";
-  document.documentElement.dataset.mode = mode;
   localStorage.setItem("mode", mode); 
+  updateTheme(mode);
 });
-
